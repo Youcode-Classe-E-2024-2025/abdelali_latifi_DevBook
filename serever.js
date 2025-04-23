d891e763730258ce2d37e8a765988c8994db2e0f
Latifi777 <abdelalilatifi3@gmail.com>
2025-04-23 16:02:21 +0100

    impliment server file

---
+++ serever.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const BookRoute = require('./routes/BookRoute');
+const path = require('path');
+
+const app = express();
+const PORT = 3000;
+
+app.use(bodyParser.json());
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/api/books', BookRoute);
+
+app.listen(PORT, () => {
+  console.log(`🚀 Server running at http://localhost:${PORT}`);
+});
