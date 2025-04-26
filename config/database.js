0a92295b113e33e3f760005c951fda5f0a92757d
Latifi777 <abdelalilatifi3@gmail.com>
2025-04-26 12:31:22 +0100

    final version

--- config/database.js
+++ config/database.js
@@ -1,6 +1,5 @@
 const mysql = require('mysql2');
 
-// Créer la connexion à la base de données
 const pool = mysql.createPool({
   host: 'localhost',      // Adresse de ton serveur MySQL
   user: 'root',           // Nom d'utilisateur MySQL
@@ -8,7 +7,6 @@ const pool = mysql.createPool({
   database: 'books'  // Nom de ta base de données
 });
 
-// Créer une promesse pour que la connexion soit utilisée dans une approche OOP
 const promisePool = pool.promise();
 
 module.exports = promisePool;
